4ab8f56f76761bc1d123101ad2743ad26d78b6bf
Bibek <bibek.roman1@gmail.com>
2025-02-02 19:05:50 +0545

    updated landing page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import CardSection from "@/components/card-section";
 import Header from "@/components/header";
 import HeroSection from "@/components/hero";
+import HowItWorkSection from "@/components/how-it-works-section";
 import StaticSection from "@/components/static-section";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import UsersFeedback from "@/components/users-feedback";
 import { features } from "@/data/features";
 import Image from "next/image";
+import FAQ from "@/components/faq";
+import CTASection from "@/components/cta-section";
 
 export default function Home() {
   return (
@@ -14,6 +18,10 @@ export default function Home() {
       <HeroSection />
       <CardSection />
       <StaticSection />
+      <HowItWorkSection />
+      <UsersFeedback />
+      <FAQ />
+      <CTASection />
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["randomuser.me"], // Add external image domains here
+  },
+};
 
 export default nextConfig;
